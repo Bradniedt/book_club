4d682106d22b5209e17c3cdfcf0b459721ac13fa
Brad Niedt <bradleyniedt@gmail.com>
2018-12-05 15:22:57 -0700

    adds book author join table and creates relationship between books and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
   validates_presence_of :name
+
+  has_many :book_authors
+  has_many :books, through: :book_authors
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ApplicationRecord
   before_save :titleizer
 
   has_many :reviews
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 
   def titleizer
     self.title = self.title.titleize
