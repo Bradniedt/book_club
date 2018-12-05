57caa459f6f8d8c2ec9bbca6a5c34d6af58abca7
Maddie Jones <maddyg91@gmail.com>
2018-12-04 18:05:17 -0700

    added more expectation to feature test for books

--- spec/features/user_sees_book_titles_spec.rb
+++ spec/features/user_sees_book_titles_spec.rb
@@ -4,10 +4,16 @@ describe "As a visitor" do
   describe "when I visit /books" do
     it "should see all book titles" do
       book_1 = Book.create(title: "book_1", pages: 100, author: "me")
+      book_2 = Book.create(title: "book_2", pages: 200, author: "you")
 
       visit '/books'
 
       expect(page).to have_content(book_1.title)
+      expect(page).to have_content(book_2.title)
+      expect(page).to have_content(book_1.pages)
+      expect(page).to have_content(book_2.pages)
+      expect(page).to have_content(book_1.author)
+      expect(page).to have_content(book_2.author)
     end
   end
-end 
+end
