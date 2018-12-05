56fa5a004994ae18c53b59984abf7f2f1607049c
Brad Niedt <bradleyniedt@gmail.com>
2018-12-04 21:50:24 -0700

    adds test for seeing a form when you click new

--- spec/features/user_sees_new_book_link_spec.rb
+++ spec/features/user_sees_new_book_link_spec.rb
@@ -8,4 +8,14 @@ describe "As a visitor" do
       expect(page).to have_link("Create a new book!", href: "/books/new")
     end
   end
+  describe "when I click visit /books/new" do
+    it "should take me to a page with a form to create a book" do
+      visit '/books/new'
+
+      expect(page).to have_field(:title)
+      expect(page).to have_field(:pages)
+      expect(page).to have_field(:author)
+      expect(page).to have_field(:year)
+    end
+  end
 end
