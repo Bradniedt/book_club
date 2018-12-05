11a040400f93d9095b5f94720117f0d51d95a664
Brad Niedt <bradleyniedt@gmail.com>
2018-12-05 13:17:55 -0700

    adds commit to merge and pull down from master because I need the most up to date master

--- spec/features/user_sees_new_book_link_spec.rb
+++ spec/features/user_sees_new_book_link_spec.rb
@@ -18,4 +18,19 @@ describe "As a visitor" do
       expect(page).to have_field("Year")
     end
   end
+  describe "when I click Create Book" do
+    it "takes me to the book's show page" do
+
+      visit new_book_path
+
+      fill_in 'Title', with: "Book 1"
+      fill_in 'Pages', with: 100
+      fill_in 'Author', with: "Name 1"
+      fill_in 'Year', with: 1970
+      click_button 'Create Book'
+
+      expect(current_path).to eq("/book/#{Book.last.id}")
+      expect(page).to have_content(Book.last.title)
+    end
+  end
 end
