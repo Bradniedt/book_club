210d1b0a1e61d3767c7153bb7083e0cf5349119c
Maddie Jones <maddyg91@gmail.com>
2018-12-05 12:54:29 -0700

    created reviews table, book to reviews relation and model tests for views

--- spec/features/user_sees_book_page_spec.rb
+++ spec/features/user_sees_book_page_spec.rb
@@ -4,12 +4,16 @@ describe "As a visitor" do
   describe "when I visit /books/show" do
     it "should see all book info" do
       book_1 = Book.create(title: "book_1", pages: 100, author: "me", year: 2018)
+      review_1 = book_1.reviews.create(title: "review_1", description: "ehfladhflad", rating: 4)
 
       visit book_path(book_1)
 
       expect(page).to have_content(book_1.title)
       expect(page).to have_content(book_1.pages)
       expect(page).to have_content(book_1.author)
+      expect(page).to have_content(review_1.title)
+      expect(page).to have_content(review_1.description)
+      expect(page).to have_content(review_1.rating)
     end
   end
 end
