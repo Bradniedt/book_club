f4292f3e9ef1768f31ff9fa15ae6ea0d6ef766fb
Brad Niedt <bradleyniedt@gmail.com>
2018-12-05 17:06:58 -0700

    adds method to book controller to create author upon book creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,16 +9,21 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @authors = @book.authors
   end
 
   def create
     book = Book.create(book_params)
+    author_array = params[:book]["authors"].split(",")
+    authors_array.each do |author|
+      book.authors << Author.find_or_create_by(:name => aut)
+    end
     redirect_to book_path(book)
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :pages, :author, :year)
+    params.require(:book).permit(:title, :pages)
   end
 end
