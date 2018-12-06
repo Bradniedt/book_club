3b029782eb78373578050f89260545b22ad3ccf8
Maddie Jones <maddyg91@gmail.com>
2018-12-06 16:42:39 -0700

    committing so maddie can leave

--- spec/features/user_can_sort_books_spec.rb
+++ spec/features/user_can_sort_books_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 describe "as a user" do
   describe "when I visit /books" do
-    describe "when I click on link for average rating ascending" do
-      it "should show books sorted by rating" do
+    describe "when I click on a sorting link" do
+      it "should show books sorted by ascending rating" do
         book_1 = Book.create(title: "book_1", pages: 100, year: 2000)
         book_2 = Book.create(title: "book_2", pages: 200, year: 2001)
         book_3 = Book.create(title: "book_3", pages: 240, year: 2004)
@@ -21,7 +21,27 @@ describe "as a user" do
         expect(all('.book')[0]).to have_content(book_3.title)
         expect(all('.book')[1]).to have_content(book_2.title)
         expect(all('.book')[2]).to have_content(book_1.title)
-        
+
+      end
+      it "should show books sorted by descending rating" do
+        book_1 = Book.create(title: "book_1", pages: 100, year: 2000)
+        book_2 = Book.create(title: "book_2", pages: 200, year: 2001)
+        book_3 = Book.create(title: "book_3", pages: 240, year: 2004)
+        review_1 = book_1.reviews.create(title: "good book" , description:"amazing", rating: 5)
+        review_2 = book_1.reviews.create(title: "bad book" , description:"lame", rating: 1)
+        review_3 = book_2.reviews.create(title: "good book" , description:"amazing", rating: 5)
+        review_4 = book_2.reviews.create(title: "not great" , description:"blah", rating: 3)
+        review_5 = book_3.reviews.create(title: "good book" , description:"amazing", rating: 4)
+        review_6 = book_3.reviews.create(title: "not great" , description:"blah", rating: 1)
+
+        visit books_path
+
+        click_on("Rating: Highest to Lowest")
+
+        expect(all('.book')[0]).to have_content(book_2.title)
+        expect(all('.book')[1]).to have_content(book_1.title)
+        expect(all('.book')[2]).to have_content(book_3.title)
+
       end
     end
   end
