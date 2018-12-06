8c183927dd95b3b45ec8e7acb8fd981257d16e0b
Maddie Jones <maddyg91@gmail.com>
2018-12-05 19:00:36 -0700

    added tests to show that page can show all books from all authors

--- spec/features/user_sees_all_books_in_author_spec.rb
+++ spec/features/user_sees_all_books_in_author_spec.rb
@@ -14,5 +14,21 @@ describe 'as a visitor' do
       expect(page).to have_content(book_1.pages)
       expect(page).to have_content(book_1.year)
     end
+    it 'should see all book titles by that author' do
+      author_1= Author.create(name: "Jones")
+      book_1 = author_1.books.create(title: "book_1", pages: 100, year: 2000)
+      book_2 = author_1.books.create(title: "book_2", pages: 100, year: 2001)
+
+      visit author_path(author_1)
+
+      expect(page).to have_content(author_1.name)
+      expect(page).to have_content(book_1.title)
+      expect(page).to have_content(book_1.pages)
+      expect(page).to have_content(book_1.year)
+      expect(page).to have_content(book_2.title)
+      expect(page).to have_content(book_2.pages)
+      expect(page).to have_content(book_2.year)
+    end
+
   end
 end
