e2c491319e599c4fe8e21c4941defa6b6b26e826
Maddie Jones <maddyg91@gmail.com>
2018-12-06 20:21:35 -0700

    added test for new review link, creating pull request because I need code from master

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'home#index'
 
-  resources :books, only: [:index, :show, :edit, :new, :create, :destroy]
+  resources :books do
+    resources :reviews, only: [:index, :show, :new, :destroy]
+  end
+
   resources :authors, only: [:show, :destroy]
+  
 end
---
+++ spec/features/user_can_write_new_review_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  describe 'when I visit /books/show' do
+    it 'I see a link to add a new review' do
+      book_1 = Book.create(title: "book_1", pages: 100, year: 2000)
+      visit book_path(book_1)
+      save_and_open_page
+
+        expect(page).to have_link("Add Review")
+    end
+  end
+end
