a4474c911a85d5833446f649e909f21646729d57
Maddie Jones <maddyg91@gmail.com>
2018-12-06 21:11:26 -0700

    added link to review

--- spec/features/user_can_write_new_review_spec.rb
+++ spec/features/user_can_write_new_review_spec.rb
@@ -4,9 +4,8 @@ describe 'as a visitor' do
   describe 'when I visit /books/show' do
     it 'I see a link to add a new review' do
       book_1 = Book.create(title: "book_1", pages: 100, year: 2000)
-      visit book_path(book_1)
-      save_and_open_page
 
+      visit book_path(book_1)
         expect(page).to have_link("Add Review")
     end
   end
