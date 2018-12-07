e502f164833d69f9e535aad63540eac6c3bec298
Brad Niedt <bradleyniedt@gmail.com>
2018-12-06 17:40:57 -0700

    adds test for number of pages method

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Book, type: :model do
   end
 
   describe 'class method' do
-    it "avg_rating_asc" do
+    it "avg_rating()" do
       book_1 = Book.create!(title: "book_1", pages: 100, year: 2000)
       book_2 = Book.create!(title: "book_2", pages: 200, year: 2001)
       book_3 = Book.create!(title: "book_3", pages: 240, year: 2004)
@@ -28,20 +28,16 @@ RSpec.describe Book, type: :model do
       review_6 = book_3.reviews.create!(title: "not great" , description:"blah", rating: 1)
 
       expect(Book.avg_rating("asc")).to eq([book_3, book_1, book_2])
+      expect(Book.avg_rating("desc")).to eq([book_2, book_1, book_3])
    end
 
-    it "avg_rating_desc" do
-      book_1 = Book.create!(title: "book_1", pages: 100, year: 2000)
-      book_2 = Book.create!(title: "book_2", pages: 200, year: 2001)
-      book_3 = Book.create!(title: "book_3", pages: 240, year: 2004)
-      review_1 = book_1.reviews.create!(title: "good book" , description:"amazing", rating: 5)
-      review_2 = book_1.reviews.create!(title: "bad book" , description:"lame", rating: 1)
-      review_3 = book_2.reviews.create!(title: "good book" , description:"amazing", rating: 5)
-      review_4 = book_2.reviews.create!(title: "not great" , description:"blah", rating: 3)
-      review_5 = book_3.reviews.create!(title: "good book" , description:"amazing", rating: 4)
-      review_6 = book_3.reviews.create!(title: "not great" , description:"blah", rating: 1)
+   it "number_of_pages()" do
+     book_1 = Book.create!(title: "book_1", pages: 500, year: 2000)
+     book_2 = Book.create!(title: "book_2", pages: 200, year: 2001)
+     book_3 = Book.create!(title: "book_3", pages: 240, year: 2004)
 
-      expect(Book.avg_rating("desc")).to eq([book_2, book_1, book_3])
+     expect(Book.number_of_pages("asc")).to eq([book_2, book_3, book_1])
+     expect(Book.number_of_pages("desc")).to eq([book_1, book_3, book_2])
    end
   end
 end
