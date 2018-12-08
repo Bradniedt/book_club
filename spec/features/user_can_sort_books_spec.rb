5e434b8388dd8ea7012b952d11a6a084b9d99a36
Maddie Jones <maddyg91@gmail.com>
2018-12-08 10:54:00 -0700

    Added test for Highest rated books

--- spec/features/user_can_sort_books_spec.rb
+++ spec/features/user_can_sort_books_spec.rb
@@ -88,6 +88,44 @@ describe "as a user" do
           expect(all('.book')[2]).to have_content(book_1.title)
         end
       end
+      describe 'I see a book statistics section' do
+        it 'should show book stats' do
+          book_1 = Book.create!(title: "book_1", pages: 100, year: 2000)
+          book_2 = Book.create!(title: "book_2", pages: 200, year: 2001)
+          book_3 = Book.create!(title: "book_3", pages: 240, year: 2004)
+          book_4 = Book.create!(title: "book_4", pages: 250, year: 2003)
+          book_5 = Book.create!(title: "book_5", pages: 1000, year: 2015)
+          book_6 = Book.create!(title: "book_6", pages: 1050, year: 2016)
+          user_1 = User.create(name: "Person One")
+          user_2 = User.create(name: "Person Two")
+          user_3 = User.create(name: "Person Three")
+          user_4 = User.create(name: "Person Four")
+          review_1 = user_1.reviews.create(title: "good book" , description:"amazing", rating: 5)
+          review_2 = user_2.reviews.create(title: "bad book" , description:"lame", rating: 1)
+          review_7 = user_3.reviews.create(title: "bad book" , description:"lame", rating: 4)
+          book_1.reviews << [review_1, review_7]
+          review_3 = user_1.reviews.create(title: "good book" , description:"amazing", rating: 5)
+          review_4 = user_2.reviews.create(title: "not great" , description:"blah", rating: 3)
+          review_8 = user_3.reviews.create(title: "not great" , description:"blah", rating: 5)
+          book_2.reviews << [review_2, review_3, review_4, review_8]
+          review_5 = user_3.reviews.create(title: "not great" , description:"blah", rating: 3)
+          review_6 = user_4.reviews.create(title: "not great" , description:"blah", rating: 4)
+          book_3.reviews << [review_5, review_6]
+          review_9 = user_3.reviews.create(title: "not great" , description:"blah", rating: 2)
+          book_4.reviews << [review_9]
+          review_10 = user_2.reviews.create(title: "not great" , description:"blah", rating: 3)
+          book_5.reviews << [review_10]
+          review_11 = user_2.reviews.create(title: "not great" , description:"blah", rating: 1)
+          book_6.reviews << [review_10]
+
+          visit books_path
+
+            within ".highest_rated_books" do
+              expect(page).to have_content("#{book_2.title} #{book_1.title} #{book_3.title}")
+            end
+
+        end
+      end
     end
   end
 end
