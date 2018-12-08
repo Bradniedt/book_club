7fed5415c229561050adebeb47ec24a04b77b5e9
Brad Niedt <bradleyniedt@gmail.com>
2018-12-07 17:44:05 -0700

    adds modification to new review test to create user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,8 @@ class ReviewsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     review = book.reviews.create(review_params)
+    user_name = params[:review]["username"].titleize
+    review.user << User.find_or_create_by(:name => user_name)
     redirect_to book_path(book)
   end
 
--- spec/features/user_sees_a_form_for_new_review_spec.rb
+++ spec/features/user_sees_a_form_for_new_review_spec.rb
@@ -8,14 +8,18 @@ describe 'as a visitor' do
       visit new_book_review_path(book_1)
 
       fill_in 'Title', with: "Would suggest"
+      fill_in 'Username', with: "my name"
       fill_in 'Description', with: "Good book"
       fill_in 'Rating', with: 4
 
       click_button 'Create Review'
 
+      user = User.last.name
+
       expect(page).to have_content("Would suggest")
       expect(page).to have_content("Good book")
       expect(page).to have_content(4)
+      expect(user.name).to eq("My Name")
     end
   end
 end
