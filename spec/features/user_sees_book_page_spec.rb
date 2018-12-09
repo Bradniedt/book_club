d866e65df9ccb537239bc538313def4ca2406b73
Brad Niedt <bradleyniedt@gmail.com>
2018-12-09 16:48:10 -0700

    adds feature test for seeing bottom three reviews in book show page

--- spec/features/user_sees_book_page_spec.rb
+++ spec/features/user_sees_book_page_spec.rb
@@ -61,5 +61,31 @@ describe "As a visitor" do
         expect(page).to have_content(review_4.user.name)
       end
     end
+
+    it "should display the top three reviews" do
+      book_1 = Book.create(title: "book_1", pages: 100, year: 1900)
+      user_1 = User.create(name: "Person One")
+      user_2 = User.create(name: "Person Two")
+      user_3 = User.create(name: "Person Three")
+      user_4 = User.create(name: "Person Four")
+      review_1 = user_1.reviews.create(title: "good book" , description:"amazing", rating: 1, book: book_1)
+      review_2 = user_2.reviews.create(title: "bad book" , description:"lame", rating: 2, book: book_1)
+      review_3 = user_3.reviews.create(title: "bad book" , description:"lame", rating: 4, book: book_1)
+      review_4 = user_4.reviews.create(title: "bad book" , description:"lame", rating: 3, book: book_1)
+
+      visit book_path(book_1)
+
+      within('.bottom-reviews') do
+        expect(page).to have_content(review_1.title)
+        expect(page).to have_content(review_1.rating)
+        expect(page).to have_content(review_1.user.name)
+        expect(page).to have_content(review_2.title)
+        expect(page).to have_content(review_2.rating)
+        expect(page).to have_content(review_2.user.name)
+        expect(page).to have_content(review_4.title)
+        expect(page).to have_content(review_4.rating)
+        expect(page).to have_content(review_4.user.name)
+      end
+    end
   end
 end
