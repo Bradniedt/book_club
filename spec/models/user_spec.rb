c17573ec073b32715c2c8c578a6b987d2b61487a
Brad Niedt <bradleyniedt@gmail.com>
2018-12-08 18:18:17 -0700

    adds test for user model review count

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:reviews)}
   end
+
+  describe 'instance methods' do
+    it 'review_count' do
+      user_1 = User.create(name: "Person One")
+      review_1 = user_1.reviews.create(title: "good book" , description:"amazing", rating: 5)
+      review_2 = user_1.reviews.create(title: "bad book" , description:"lame", rating: 1)
+      review_7 = user_1.reviews.create(title: "bad book" , description:"lame", rating: 4)
+
+      expect(user_1.review_count).to eq(3)
+    end
+  end
 end
