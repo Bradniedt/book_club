cba9b0341b00b3f32129c9817535b996081e3564
Brad Niedt <bradleyniedt@gmail.com>
2018-12-08 19:36:28 -0700

    adds feature test for all usernames are links

--- spec/features/user_sees_book_page_spec.rb
+++ spec/features/user_sees_book_page_spec.rb
@@ -20,5 +20,20 @@ describe "As a visitor" do
       expect(page).to have_content(review_1.description)
       expect(page).to have_content(review_1.rating)
     end
+
+    it 'should see all user names as links to that user show page' do
+      book_1 = Book.create(title: "book_1", pages: 100, year: 1900)
+      user_1 = User.create(name: "Person One")
+      user_2 = User.create(name: "Person Two")
+      review_1 = user_1.reviews.create(title: "review_1", description: "ehfladhflad", rating: 4)
+      review_2 = user_2.reviews.create(title: "review_2", description: "ehfladhflad", rating: 5)
+      book_1.reviews << [review_1, review_2]
+      author_1 = book_1.authors.create(name: "Author One")
+
+      visit book_path(book_1)
+
+      expect(page).to have_link("Person One", user_path(user_1))
+      expect(page).to have_link("Person Two", user_path(user_2))
+    end
   end
 end
