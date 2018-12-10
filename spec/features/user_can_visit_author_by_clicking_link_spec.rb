507dcfc58cf63102436a3096284c7d1d69af3d73
Maddie Jones <maddyg91@gmail.com>
2018-12-10 13:18:53 -0700

    Added top review to each book from author. Tests passing

--- spec/features/user_can_visit_author_by_clicking_link_spec.rb
+++ spec/features/user_can_visit_author_by_clicking_link_spec.rb
@@ -18,8 +18,10 @@ describe 'as a visitor' do
     it "should show author's top review" do
       book_1 = Book.create(title: "book_1", pages: 100, year: 1900)
       user_1 = User.create(name: "Person One")
+      user_2 = User.create(name: "Person Two")
+
       review_1 = user_1.reviews.create(title: "review_1", description: "ehfladhflad", rating: 1, book: book_1)
-      review_2 = user_1.reviews.create(title: "review_2", description: "ehfladhflad", rating: 5, book: book_1)
+      review_2 = user_2.reviews.create(title: "review_2", description: "ehfladhflad", rating: 5, book: book_1)
       author_1 = book_1.authors.create(name: "Author One")
 
       visit author_path(author_1)
