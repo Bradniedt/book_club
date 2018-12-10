fc89f6ec93dabbbcf17c7da3e8b9d5756a78a6d0
Brad Niedt <bradleyniedt@gmail.com>
2018-12-10 14:34:53 -0700

    adds expectation to author delete test for deleting a book with two authors

--- spec/features/user_can_visit_author_by_clicking_link_spec.rb
+++ spec/features/user_can_visit_author_by_clicking_link_spec.rb
@@ -41,14 +41,15 @@ describe 'as a visitor' do
       expect(page).to have_link("Delete Author")
     end
     it "can delete an author when user clicks delete author" do
-      book_1 = Book.create(title: "book_1", pages: 100, year: 1900)
-      book_2 = Book.create(title: "book_2", pages: 200, year: 2000)
       user_1 = User.create(name: "Person One")
       user_2 = User.create(name: "Person Two")
+      author_1 = Author.create(name: "Author One")
+      author_2 = Author.create(name: "Author Two")
+      book_1 = Book.create(title: "book_1", pages: 100, year: 1900, authors: [author_1, author_2])
+      book_2 = Book.create(title: "book_2", pages: 200, year: 2000, authors: [author_2])
 
       review_1 = user_1.reviews.create(title: "review_1", description: "ehfladhflad", rating: 1, book: book_1)
       review_2 = user_2.reviews.create(title: "review_2", description: "ehfladhflad", rating: 5, book: book_1)
-      author_1 = book_1.authors.create(name: "Author One")
 
       visit author_path(author_1)
 
@@ -59,6 +60,7 @@ describe 'as a visitor' do
       expect(page).to_not have_content(author_1.name)
       expect(page).to_not have_content(book_1.title)
       expect(page).to have_content(book_2.title)
+      expect(page).to have_content(book_2.authors[0].name)
     end
   end
 end
