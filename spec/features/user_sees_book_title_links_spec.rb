4d759067d8cba032c4837e3dbcd17618b853fa66
Brad Niedt <bradleyniedt@gmail.com>
2018-12-11 18:46:21 -0700

    adds links to readme and adds rounding to book index and show page and fixes sort links to show books without reviews

--- spec/features/user_sees_book_title_links_spec.rb
+++ spec/features/user_sees_book_title_links_spec.rb
@@ -1,8 +1,11 @@
 describe "as a user" do
   describe "when I see a book title" do
     it "is a link to the book's show page" do
-      book_1 = Book.create(title: "book_1", pages: 100, year: 1900)
-
+      author_1 = Author.create(name: "Joe")
+      book_1 = author_1.books.create(title: "book_1", pages: 100, year: 1900)
+      user_1 = User.create(name: "Bob")
+      review_1 = user_1.reviews.create(title: "good book" , description:"amazing", rating: 5, book: book_1)
+      
       visit books_path
 
       expect(page).to have_link("Book 1")
